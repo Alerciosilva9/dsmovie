26e57a966550abef78bf4a8e927a84af78c407b6
Alercio Silva <silvaalercio@gmail.com>
2022-01-14 20:47:47 -0300

    Fix Navbar path error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,9 @@ import {
 } from "react-router-dom";
 
 
-import Listing from "pages/Listing";
+import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Navbar from "components/Navbar";
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
@@ -24,12 +24,3 @@ function App() {
 }
 
 export default App;
-
-/*
-<Routes>
-        <Route path="/" element={<Listing />} />
-        <Route path="/form">
-          <Route path=":movieId" element={<Form />} />
-        </Route>
-      </Routes>
-*/
