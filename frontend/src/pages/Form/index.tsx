70e5a09d316cf897418fb705905b478874ce4021
Alercio Silva <silvaalercio@gmail.com>
2022-01-16 21:52:37 -0300

    Show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,6 @@
 import FormCard from 'components/FormCard';
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import { useParams } from 'react-router-dom';
+
 
 function Form(){
 
@@ -13,3 +13,4 @@ function Form(){
 }
 
 export default Form;
+
