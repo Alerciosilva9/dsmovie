b1c69ad103ed75e0d609a07b5bfa13352f733939
Alercio Silva <silvaalercio@gmail.com>
2022-01-17 02:01:39 -0300

    Fix baseurl bug 2.0

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "appdsmovie.herokuapp.com";
+export const BASE_URL = "https://appdsmovie.herokuapp.com";
 
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
